151e58344e48a601a2571fbcc0179e6b90d562de
njelich <12912633+njelich@users.noreply.github.com>
2022-01-24 20:28:21 +0000

    updated voting engine

--- bridge/VotingEngine.sol
+++ bridge/VotingEngine.sol
@@ -7,17 +7,45 @@ import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
-import "../interfaces/IERC20Base.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../utils/Signature.sol";
 
 contract VotingEngine is
+  Initializable,
   PausableUpgradeable,
   ReentrancyGuardUpgradeable,
   OwnableUpgradeable,
   Signature
 {
+  event Deposited(address indexed user, uint256 amount);
+  event Withdrawn(address indexed user, uint256 amount);
+
+  event VoteProposed(uint256 indexed proposal, uint256 rewards);
+  event VoteResolved(uint256 indexed proposal, string ipfs);
+
+  // Token logic
+  IERC20 public bct;
+
+  // Withdrawal logic
   mapping(address => uint256) public nonceMap;
   mapping(address => uint256) public withdrawn;
+
+  // Voting logic
+  uint256 internal _voteCounter = 0;
+  mapping(uint256 => string) internal _voteResolutionHash;
+
+  function initialize(
+        address bctContractAddress_
+    ) external initializer {
+        __Context_init_unchained();
+        __Pausable_init_unchained();
+        __Ownable_init_unchained();
+        __ReentrancyGuard_init_unchained();
+
+        bct = IERC20(bctContractAddress_);
+
+        _pause();
+    }
 
   function withdraw(
     uint256 amount,
@@ -32,9 +60,50 @@ contract VotingEngine is
     // Current nonce has to match this one to avoid double withdrawals
     require(nonceMap[msg.sender] == nonce, "Invalid nonce");
 
-    //TODO: transfer logic
-
     nonceMap[msg.sender]++;
     withdrawn[msg.sender] += amount;
+
+    emit Withdrawn(_msgSender(), amount);
+
+    bct.transfer(msg.sender, amount);
+  }
+  
+  function deposit(
+    uint256 amount
+  ) external {
+    bct.transferFrom(
+            _msgSender(),
+            address(this),
+            amount
+        );
+
+    emit Deposited(_msgSender(), amount);
+  }
+
+  function proposeVote(uint256 rewardAmount) external returns (uint256 id) {
+    uint256 voteId = _voteCounter;
+
+    bct.transferFrom(
+            _msgSender(),
+            address(this),
+            rewardAmount
+        );
+
+    _voteCounter++;
+    emit VoteProposed(voteId, rewardAmount);
+
+    return voteId;
+  }
+
+  function resolveVote(uint256 id, string memory ipfs) external onlyOwner  {
+    // add immutability condition, so it can be resolved only once
+    // require(_voteResolutionHash[id]);
+    _voteResolutionHash[id] = ipfs;
+
+    emit VoteResolved(id, ipfs);
+  }
+    //Returns the ipfs hash associated with a vote's results.
+  function getVoteState(uint256 id) external view returns (string memory ipfs) {
+    return _voteResolutionHash[id];
   }
 }
--- interfaces/IVotingEngine.sol
+++ interfaces/IVotingEngine.sol
@@ -7,16 +7,16 @@ interface IVotingEngine {
     event Withdrawn(address indexed user, uint256 amount);
 
     event VoteProposed(uint256 indexed proposal, uint256 rewards);
-    event VoteResolved(uint256 indexed proposal, uint256 rewards);
+    event VoteResolved(uint256 indexed proposal, string ipfs);
 
     //User facing deposit and withdraw functions, deposit emits a Deposited event
-    function deposit(uint256 amount) external returns (uint256 balance);
+    function deposit(uint256 amount) external;
     //We sign the withdrawal request, moving the gas cost onto the user
     function withdraw(
         uint256 amount,
         uint256 nonce,
         bytes calldata signature
-    ) external returns (uint256 balance);
+    ) external;
 
     //User facing proposeVote with a reward amount (minimum exists)
     function proposeVote(uint256 rewardAmount) external returns (uint256 id);
