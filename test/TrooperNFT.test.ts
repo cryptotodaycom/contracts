5c3469e9c130482b8c5872b3dc5e9cd83e061f6e
njelich <12912633+njelich@users.noreply.github.com>
2022-02-13 17:15:17 +0100

    trooper nft contract

---
+++ test/TrooperNFT.test.ts
@@ -0,0 +1,46 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+// eslint-disable-next-line node/no-missing-import
+import { TrooperNFT } from "../typechain";
+
+const address0 = ethers.constants.AddressZero;
+
+describe("testing trooper nft minting", async function () {
+  let owner: any, user1: any, user2: any;
+  let trooperNft: TrooperNFT;
+
+  before(async () => {
+    [owner, user1, user2] = await ethers.getSigners();
+
+    // Deploy trooper NFT contract
+    const TrooperNft = await ethers.getContractFactory("TrooperNFT");
+    trooperNft = (await TrooperNft.deploy()) as TrooperNFT;
+    await trooperNft.deployed();
+  });
+
+  // emitted event is TransferSingle(operator, address(0), to, id, amount);
+
+  describe("user interactions", async function () {
+    it("should allow airdrop minting", async function () {
+      // should emit minted event
+      await expect(trooperNft.airdrop(user2.address, 1)).to.emit(trooperNft, "TransferSingle").withArgs(owner.address, address0, user2.address, 1, 1);
+    });
+
+    it("should reject wrong signature", async function () {
+      const payload = ethers.utils.arrayify(ethers.utils.solidityKeccak256(["address"], [user2.address]));
+      const signature = ethers.utils.arrayify(await owner.signMessage(payload));
+
+      await expect(trooperNft.connect(user1).claimTrooper(signature)).to.be.revertedWith("Invalid signature");
+    });
+
+    it("should allow claiming (only once per address)", async function () {
+      const payload = ethers.utils.arrayify(ethers.utils.solidityKeccak256(["address"], [user1.address]));
+      const signature = ethers.utils.arrayify(await owner.signMessage(payload));
+
+      await expect(trooperNft.connect(user1).claimTrooper(signature))
+        .to.emit(trooperNft, "TransferSingle")
+        .withArgs(user1.address, address0, user1.address, 0, 1);
+      await expect(trooperNft.connect(user1).claimTrooper(signature)).to.be.revertedWith("Already claimed");
+    });
+  });
+});
--- typechain/hardhat.d.ts
+++ typechain/hardhat.d.ts
@@ -29,6 +29,22 @@ declare module "hardhat/types/runtime" {
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.Pausable__factory>;
     getContractFactory(
+      name: "ERC1155",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.ERC1155__factory>;
+    getContractFactory(
+      name: "IERC1155MetadataURI",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.IERC1155MetadataURI__factory>;
+    getContractFactory(
+      name: "IERC1155",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.IERC1155__factory>;
+    getContractFactory(
+      name: "IERC1155Receiver",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.IERC1155Receiver__factory>;
+    getContractFactory(
       name: "ERC20",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.ERC20__factory>;
@@ -57,6 +73,10 @@ declare module "hardhat/types/runtime" {
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.IERC721__factory>;
     getContractFactory(
+      name: "ERC165",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.ERC165__factory>;
+    getContractFactory(
       name: "IERC165",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.IERC165__factory>;
@@ -76,6 +96,14 @@ declare module "hardhat/types/runtime" {
       name: "Signature",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.Signature__factory>;
+    getContractFactory(
+      name: "SignatureNFT",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.SignatureNFT__factory>;
+    getContractFactory(
+      name: "TrooperNFT",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.TrooperNFT__factory>;
     getContractFactory(
       name: "VotingEngine",
       signerOrOptions?: ethers.Signer | FactoryOptions
@@ -101,6 +129,26 @@ declare module "hardhat/types/runtime" {
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.Pausable>;
+    getContractAt(
+      name: "ERC1155",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.ERC1155>;
+    getContractAt(
+      name: "IERC1155MetadataURI",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.IERC1155MetadataURI>;
+    getContractAt(
+      name: "IERC1155",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.IERC1155>;
+    getContractAt(
+      name: "IERC1155Receiver",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.IERC1155Receiver>;
     getContractAt(
       name: "ERC20",
       address: string,
@@ -137,6 +185,11 @@ declare module "hardhat/types/runtime" {
       signer?: ethers.Signer
     ): Promise<Contracts.IERC721>;
     getContractAt(
+      name: "ERC165",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.ERC165>;
+    getContractAt(
       name: "IERC165",
       address: string,
       signer?: ethers.Signer
@@ -161,6 +214,16 @@ declare module "hardhat/types/runtime" {
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.Signature>;
+    getContractAt(
+      name: "SignatureNFT",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.SignatureNFT>;
+    getContractAt(
+      name: "TrooperNFT",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.TrooperNFT>;
     getContractAt(
       name: "VotingEngine",
       address: string,
