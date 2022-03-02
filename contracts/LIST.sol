f84c2769d215fd693b8b8c7b5173d1bbdfb960cf
njelich <12912633+njelich@users.noreply.github.com>
2022-03-02 10:36:00 +0100

    removed unused import

--- contracts/LIST.sol
+++ contracts/LIST.sol
@@ -2,7 +2,6 @@
 
 pragma solidity 0.8.12;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
